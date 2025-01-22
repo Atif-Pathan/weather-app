f70fe4fcbde0c56ddb5fdca46ae250c160c7d128
Atif Pathan <atifkhan.p1@gmail.com>
2025-01-21 23:31:08 -0700

    update index.js to handle updating all values based on search and some minor styling

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import './styles.css';
-// import { fetchWeather } from './modules/weatherAPI';
+import { fetchWeather } from './modules/weatherAPI';
 import displayCurrentConditions from './modules/displayCurrentConditions';
-import test from '../test.json';
+// import test from '../test.json';
+// import './modules/carouselForecast';
+import carousel from './modules/carouselForecast';
+import displayForecast from './modules/displayForecast';
 
 const searchBar = document.getElementById('search-input');
 const searchBtn = document.querySelector('.search-btn');
@@ -9,13 +12,15 @@ const searchBtn = document.querySelector('.search-btn');
 searchBtn.addEventListener('click', async () => {
   console.log(searchBar.value);
   try {
-    // const location = searchBar.value;
-    // const weatherData = await fetchWeather(location, 'metric');
-    const weatherData = test;
+    const location = searchBar.value;
+    const weatherData = await fetchWeather(location, 'metric');
+    // const weatherData = test;
     displayCurrentConditions(
       weatherData.currentConditions,
       weatherData.resolvedAddress
     );
+    displayForecast(weatherData.days);
+    carousel.resetCarousel();
   } catch (error) {
     console.log(error);
   }
